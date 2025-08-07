9285cafdc453e70e0129eadab33c2956db3baf98
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2025-08-07 16:59:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, HTMLResponse
+from starlette.middleware.sessions import SessionMiddleware
+
 
 app  = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key = 'secreto')
 templates = Jinja2Templates(directory="templates")
 
 async def home(request: Request, name: str = Form(None), email: str = Form(None), password: str = Form(None)):
     print(name, email, password)
-    return templates.TemplateResponse('home.html', {"request": request, "username": name})
+
+    request.session['username'] = name
+
+    username = request.session['username']
+
+    return templates.TemplateResponse('home.html', {"request": request, "username": username})
 
 async def index(request: Request):
     if request.method == "POST":
